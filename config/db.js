32f2974d34ca4c269b35468b779241b606178751
Tobi <tobisaid007@gmail.com>
2025-01-24 17:24:54 +0100

    fixes

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-
+mongoose.set('strictQuery', true);
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
-}
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log(`MongoDB Connected: ${conn.connection.host}`);
+};
 
-module.exports = connectDB
+module.exports = connectDB;
